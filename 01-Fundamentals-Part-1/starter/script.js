b650353d8d9f8696ff65a17799878835edb3e35b
hamidrezaHemati <hrhhemati@gmail.com>
2025-10-01 09:04:28 +0330

    challenge 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,29 @@
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+const dolphins_score_1 = 97
+const dolphins_score_2 = 112
+const dolphins_score_3 = 101
 
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
+const koalas_score_1 = 109
+const koalas_score_2 = 95
+const koalas_score_3 = 106
 
-if (BMIMark > BMIJohn) {
-  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
-} else {
-  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+const dolphins_avg = (dolphins_score_1 + dolphins_score_2 + dolphins_score_3) / 3
+const koalas_avg = (koalas_score_1 + koalas_score_2 + koalas_score_3) / 3
+console.log(dolphins_avg, koalas_avg)
+
+let hasDolphinsMinimumReq = dolphins_avg >= 100
+let hasKoalasMinimumReq = koalas_avg >= 100
+
+if (dolphins_avg == koalas_avg){
+  if (hasDolphinsMinimumReq && hasKoalasMinimumReq) console.log("Draw")
+  else console.log('No winners!')
 }
+else if(dolphins_avg > koalas_avg){
+  if (hasDolphinsMinimumReq) console.log("dolphins Win")
+  else console.log('Dolphins has the higher score but doesnt Won!')
+}
+else{
+  if (hasDolphinsMinimumReq) console.log("koalas win")
+  else console.log('Koalas has the higher score but doesnt Won!')
+  
+}
+
