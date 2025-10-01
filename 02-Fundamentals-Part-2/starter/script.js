3e6b636adcbd53112c661bb5d6ea65e05ccf346b
hamidrezaHemati <hrhhemati@gmail.com>
2025-10-01 11:48:49 +0330

    Challenge 2.5

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,23 +1,11 @@
-bills = new Array(125,555,44)
-tips = new Array()
-totlas = new Array()
-
-const calcTip = bill => bill <=300 && bill >= 50 ? bill * 0.15 : bill * 0.2
-const calcTip2 = function(bill){
-    return bill <=300 && bill >= 50 ? bill * 0.15 : bill * 0.2
-}
-function calcTip3(bill){
-    return bill <=300 && bill >= 50 ? bill * 0.15 : bill * 0.2
+const Hamid = {
+    firstName: 'Hamidreza',
+    lastName: 'Hemati',
+    friends: ['Parvin', 'Kasra', 'Niusha'],
+    location: 'Iran',
+    job: 'Developer'
 }
 
-tips.push(calcTip3(bills[0]))
-tips.push(calcTip3(bills[1]))
-tips.push(calcTip3(bills[2]))
+console.log(Hamid['job'])
 
-totlas.push(bills[0] + tips[0])
-totlas.push(bills[1] + tips[1])
-totlas.push(bills[2] + tips[2])
-
-console.log(bills)
-console.log(tips)
-console.log(totlas)
+console.log(`${Hamid.firstName} has ${Hamid.friends.length}, and his best frind called ${Hamid['friends'][0]}`)
